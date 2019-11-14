d0cc759d94e8f39553d2eac7a4079f9d6c5edc71
We686 <1256092936@qq.com>
2019-11-14 21:14:56 +0800

    文章详情页实现

--- src/main/java/com/qf/jianshu/JianshuApplication.java
+++ src/main/java/com/qf/jianshu/JianshuApplication.java
@@ -1,9 +1,11 @@
 package com.qf.jianshu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qf.jianshu.mapper")
 public class JianshuApplication {
 
     public static void main(String[] args) {
